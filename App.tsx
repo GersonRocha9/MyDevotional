a75f20b80ea5115bf317397874946e88ccba19e8
Gerson Rocha <gersonrocha9@gmail.com>
2024-07-17 19:27:16 -0300

    feat: add satochi font

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
 import { AppRoutes } from '@routes'
 import { ThemeProvider } from '@shopify/restyle'
 import { theme } from '@theme'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
+SplashScreen.preventAutoHideAsync()
+
 export default function App() {
+  const [loaded, error] = useFonts({
+    'Satoshi-Black': require('./assets/fonts/Satoshi-Black.otf'),
+    'Satoshi-BlackItalic': require('./assets/fonts/Satoshi-BlackItalic.otf'),
+    'Satoshi-Bold': require('./assets/fonts/Satoshi-Bold.otf'),
+    'Satoshi-BoldItalic': require('./assets/fonts/Satoshi-BoldItalic.otf'),
+    'Satoshi-Italic': require('./assets/fonts/Satoshi-Italic.otf'),
+    'Satoshi-Light': require('./assets/fonts/Satoshi-Light.otf'),
+    'Satoshi-LightItalic': require('./assets/fonts/Satoshi-LightItalic.otf'),
+    'Satoshi-Medium': require('./assets/fonts/Satoshi-Medium.otf'),
+    'Satoshi-MediumItalic': require('./assets/fonts/Satoshi-MediumItalic.otf'),
+    'Satoshi-Regular': require('./assets/fonts/Satoshi-Regular.otf'),
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded, error])
+
+  if (!loaded && !error) {
+    return null
+  }
+
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
